865f8fdb2b256cc1f13837255c6881c8952ec6f9
López-Cepero <2066488@cognizant.com>
2021-11-18 15:55:21 +0100

    feat(training-proyect): added repository feature

---
+++ libs/training-project/feature-repository/README.md
@@ -0,0 +1,7 @@
+# training-project-feature-repository
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test training-project-feature-repository` to execute the unit tests.
---
+++ libs/training-project/feature-repository/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'training-project-feature-repository',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory:
+    '../../../coverage/libs/training-project/feature-repository',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
